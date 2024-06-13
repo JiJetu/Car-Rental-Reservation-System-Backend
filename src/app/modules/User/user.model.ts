190b9a216b8e496e34ed9118fcfa05975e8dba64
Md Jaoadul Islam <jijetu2@gmail.com>
2024-06-13 11:44:48 +0600

    initail set up compleated

---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+import { TUser } from "./user.interface";
+import { userRole } from "./user.constant";
+
+const userSchema = new Schema<TUser>({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: userRole,
+    required: [true, "Role is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  phone: {
+    type: String,
+    required: [true, "Phone number is required"],
+  },
+  address: {
+    type: String,
+    required: [true, "Address is required"],
+  },
+});
+
+export const User = model<TUser>("User", userSchema);
