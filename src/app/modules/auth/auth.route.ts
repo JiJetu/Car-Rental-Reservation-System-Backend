6d96816b5b04d81ba38bba208856e64af0ee6d0f
Md Jaoadul Islam <jijetu2@gmail.com>
2024-06-16 02:31:15 +0600

    created valideRequest middleware to validatide request data with jot validation

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,18 +1,20 @@
 import express from "express";
 import { AuthValidation } from "./auth.validation";
 import { AuthController } from "./auth.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { UserValidations } from "../User/user.validation";
 
 const router = express.Router();
 
 router.post(
   "/signup",
-//   validateRequest(AuthValidation.loginValidationSchema),
+  validateRequest(UserValidations.createUserValidationSchema),
   AuthController.registerUser
 );
 
 router.post(
   "/signin",
-//   validateRequest(AuthValidation.loginValidationSchema),
+  validateRequest(AuthValidation.loginValidationSchema),
   AuthController.loginUser
 );
 
